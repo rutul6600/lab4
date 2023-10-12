2b920cef1b15f050b898c850e5e8569aae92d414
rutul8016 <96240213+rutul8016@users.noreply.github.com>
2023-10-11 23:08:32 -0400

    user java

---
+++ src/model/User.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author rutul
+ */
+public class User {
+    String firstname1;
+    String lastname1;
+    String age1;
+    String email1;
+    String message1;
+    String buttonGroup;
+    String patienttype1;
+    String image1;
+    private ImageIcon image10;
+
+    public String getFirstname1() {
+        return firstname1;
+    }
+
+    public void setFirstname1(String firstname1) {
+        this.firstname1 = firstname1;
+    }
+
+    public String getLastname1() {
+        return lastname1;
+    }
+
+    public void setLastname1(String lastname1) {
+        this.lastname1 = lastname1;
+    }
+
+    public String getAge1() {
+        return age1;
+    }
+
+    public void setAge1(String age1) {
+        this.age1 = age1;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getMessage1() {
+        return message1;
+    }
+
+    public void setMessage1(String message1) {
+        this.message1 = message1;
+    }
+
+    public String getButtonGroup() {
+        return buttonGroup;
+    }
+
+    public void setButtongroup(String buttongroup) {
+        this.buttonGroup = buttongroup;
+    }
+
+    public String getPatienttype1() {
+        return patienttype1;
+    }
+
+    public void setPatienttype1(String patienttype1) {
+        this.patienttype1 = patienttype1;
+    }
+
+    public String getImage1() {
+        return image1;
+    }
+
+    public void setImage1(String image1) {
+        this.image1 = image1;
+    }
+    public String getbuttonGroup(){
+        return buttonGroup;
+    }
+    public void setbuttonGroup(String buttonGroup) {
+         this.buttonGroup = buttonGroup;
+        }
+   public ImageIcon getimage10(){
+       return image10;
+   }
+   public ImageIcon setimage10(){
+       this.image10 = image10;
+        return null;
+   }
+    
+
+    
+    
+}
